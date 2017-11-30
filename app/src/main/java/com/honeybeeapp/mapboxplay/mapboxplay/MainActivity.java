79da82dae9d429e84b042c55e31ba9e8616ccebc
Diederik Hattingh <djh@emss.co.za>
2017-11-30 16:46:58 +0200

    Changes to reproduce crash.

--- app/src/main/java/com/honeybeeapp/mapboxplay/mapboxplay/MainActivity.java
+++ app/src/main/java/com/honeybeeapp/mapboxplay/mapboxplay/MainActivity.java
@@ -1,16 +1,10 @@
 package com.honeybeeapp.mapboxplay.mapboxplay;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,40 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        /*MapView map = (MapView) findViewById(R.id.mapView);
-        map.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(MapboxMap mapboxMap) {
-                Toast.makeText(MainActivity.this, R.string.map_ready, Toast.LENGTH_LONG).show();
-            }
-        });*/
+        final SupportMapFragment mapFragment1 = new SupportMapFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.map_frame_layout, mapFragment1).commit();
 
-        final SupportMapFragment mapFragment = new SupportMapFragment();
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(MapboxMap mapboxMap) {
-                Toast.makeText(MainActivity.this, R.string.map_ready, Toast.LENGTH_LONG).show();
-            }
-        });
-        getSupportFragmentManager().beginTransaction().replace(R.id.map_frame_layout, mapFragment).commit();
+        // Contrived example of mapFragment1 being destroyed before surface view added.
 
-        Button button2 = findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, NavActivity.class);
-                MainActivity.this.startActivity(i);
-            }
-        });
-
-        Button button3 = findViewById(R.id.button3);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, BottomActivity.class);
-                MainActivity.this.startActivity(i);
-            }
-        });
+        final SupportMapFragment mapFragment2 = new SupportMapFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.map_frame_layout, mapFragment2).commit();
 
     }
 
